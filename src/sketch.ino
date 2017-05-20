2ab716c9361086a5961c3231cd97d9395b92e7de
torjeikenes <torje.n.eikenes@gmail.com>
2017-05-20 19:40:08 +0200

    Tok vekk unødvendig kode

--- src/sketch.ino
+++ src/sketch.ino
@@ -1,89 +1,61 @@
-//input for pwm signal
-byte PWM_PIN = 3;
-byte PWM_PIN2 = 4;
- 
-float pwm_value;
-float avrpwm;
-float pwm;
+// Definerer input pinsene for pwm signalet fra receiveren
+int PWM_PIN = 7;
+int PWM_PIN2 = 4;
 
-//pins for led
-int pin1 = 8;
-int pin2 = 9;
-int pin3 = 10;
-
+// Definerer pinsene for høyre og venstre hjul
 int rightPin = 3;
 int leftPin = 5;
 
-float throt;
-float steer;
-float left;
-float right;
 
-//funksjon for aa faa ut gjennonsnitlig pwm signal over 5 perioder
-int getPwm(int pwmPin){
-	
-	avrpwm = 0;
-	for(int i=0; i<5; i++){
-		pwm_value = pulseIn(pwmPin, HIGH);
-		avrpwm += pwm_value;
-		delay(1);
+// Funksjon for å få ut gjennonsnitlig pwm signal over 5 perioder
+float getPwm(int pwmPin){
+	float avrpwm = 0; // Setter gjennomsnitlig pwm til 0
+	for(int i=0; i<5; i++){ // Kjører 5 ganger
+		float pwm_value = pulseIn(pwmPin, HIGH); // setter pwm_value 
+							//til lengden på en 
+							// høy puls i ms.
+		avrpwm += pwm_value; // plusser på pwm_value til avrpwm
+		delay(0.1); //veldig kort pause for å unngå error
 	}
-	//trekker fra 896 for at 0 skal vere 0
-	pwm = (avrpwm / 5)-896;
+	//trekker fra 896 for at 0 throttle på kontrolleren skal ha verdi 0
+	float pwm = (avrpwm / 5)-896;
 	return pwm;
 }
 
 void setup() {
+	// Setter PWM pinsene til input og rightpin og leftpin til output
 	pinMode(PWM_PIN, INPUT);
 	pinMode(PWM_PIN2, INPUT);
 	pinMode(rightPin, OUTPUT);
 	pinMode(leftPin, OUTPUT);
-	pinMode(pin1, OUTPUT);
-	pinMode(pin2, OUTPUT);
-	pinMode(pin3, OUTPUT);
+	// Starter seriekommunikasjon til pcen, med overføringshastiget på 9600
 	Serial.begin(9600);
 }
  
 void loop() {
-	//leser pwm fra throttle pinnen. deler på 8 for å få resultater innenfor 128 verdier
-	throt = getPwm(PWM_PIN)/8;
-	//deler på 512 for at verdien skal gå fra 0 til 2. Den funker som en multiplier
-	steer = getPwm(PWM_PIN2)/512;
-	//throt = (float)analogRead(PWM_PIN)/8;
-	//steer = (float)analogRead(PWM_PIN2)/512; 
+	//leser pwm fra throttle pinnen. deler på 8 for å få resultater 
+	//innenfor 128 verdier
+	float throt = getPwm(PWM_PIN)/8;
+	//deler på 512 for at verdien skal gå fra 0 til 2. 
+	//Den funker som en multiplier
+	float steer = getPwm(PWM_PIN2)/512;
 
-	// ganger throttle med steer. når steer er 1 vil begge hjul gå like fort. 
-	left = throt * steer;
-	//omvendt proposjonal med venstre hjul
-	right = throt * (2.0-steer);
+	// ganger throttle med steer. 
+	//når steer er 1 vil begge hjul gå like fort. 
+	float left = throt * steer;
+	// steer fungere omvendt proposjonal på høyre hjul, 
+	// i forhold til venstre hjul
+	float right = throt * (2.0-steer);
 
+	// Sender verdiene for høyre og venstre hjul over serie, 
+	//så det kan sees på pc
 	Serial.println(left);
 	Serial.println(right);
-	//Serial.println(right);
 
-	analogWrite(pin1, (int)left);
-	analogWrite(pin2, (int)right);
+	// Skriver pwm verdien for høyre og venstre hjul til utgangene.
+	analogWrite(leftPin, (int)left);
+	analogWrite(rightPin, (int)right);
+	// venter i 100 ms
 	delay(100);
-	/*
-	if (throt < 10) {
-		digitalWrite(pin1, LOW);	
-		digitalWrite(pin1, LOW);	
-		digitalWrite(pin1, LOW);	
-	}
-	else if (throt <300){
-		digitalWrite(pin1, HIGH);	
-		digitalWrite(pin2, LOW);	
-		digitalWrite(pin3, LOW);	
-	}
-	else if (throt < 600){
-		digitalWrite(pin1, HIGH);	
-		digitalWrite(pin2, HIGH);	
-		digitalWrite(pin3, LOW);	
-	}
-	else if (throt < 1000){
-		digitalWrite(pin1, HIGH);	
-		digitalWrite(pin2, HIGH);	
-		digitalWrite(pin3, HIGH);	
-	}*/
 }
 
